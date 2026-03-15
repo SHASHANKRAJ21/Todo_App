8560a70a568cec35c40a20f586dd3fd3bcb8f041
SHASHANKRAJ21 <shashank286raj@gmail.com>
2026-03-15 18:30:53 +0530

    cors again changed for vercel

--- backend/src/main/java/com/todo/todoapp/config/CorsConfig.java
+++ backend/src/main/java/com/todo/todoapp/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
 
@@ -13,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOrigins("https://todo-app-cljb.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
